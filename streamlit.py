60db9d766d4e8d01cee630926a16eb2595f02029
TheLonelyDude <magnus.kongstad@gmail.com>
2024-03-03 12:46:01 +0100

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App")
+    st.write("Welcome to my app!")
+    
+    # Add your Streamlit code here
+    
+if __name__ == '__main__':
+    main()
